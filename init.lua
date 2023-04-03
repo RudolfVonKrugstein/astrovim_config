d53127d3fa8ae3ca0cf832423e1ebb999486e9be
Nathan Hüsken <nathan@wintercloud.de>
2023-04-03 08:20:49 +0200

    configure my plugins

---
+++ init.lua
@@ -0,0 +1,22 @@
+return {
+  plugins = {
+    {
+      "ahmedkhalf/project.nvim",
+      lazy = false,
+      config = function()
+        require("project_nvim").setup {
+          detection_methods = { "pattern", "lsp" },
+          silent_chdir = false
+        }
+      end
+    },
+    {
+      "kylechui/nvim-surround",
+      lazy = false,
+      config = function()
+        require("nvim-surround").setup {
+        }
+      end
+    }
+  }
+}
