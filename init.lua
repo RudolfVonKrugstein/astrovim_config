da3ba4ec36f06726930a83602c193926ff87c56a
Nathan Hüsken <nathan@wintercloud.de>
2023-04-11 22:25:39 +0200

    add markdown preview

--- init.lua
+++ init.lua
@@ -17,6 +17,13 @@ return {
         require("nvim-surround").setup {
         }
       end
+    },
+    {
+      "iamcco/markdown-preview.nvim",
+      lazy = false,
+      config = function()
+        vim.fn["mkdp#util#install"]()
+      end
     }
   }
 }
